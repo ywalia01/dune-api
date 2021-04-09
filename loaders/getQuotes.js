6f0277d60f6b619dc0b1e309b1b8307d7c7de0df
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-10 00:38:58 +0530

    configure seperate loaders

---
+++ loaders/getQuotes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var quotes = require('../models/quotes');
+
+module.exports = {
+    getRandom: function getRandom(numberOfQuotes) {
+        var limit = numberOfQuotes > quotes.length ? quotes.length : numberOfQuotes;
+
+        var out = new Array(limit);
+        var quote;
+
+        for (var i = 0; i < limit; i++) {
+            do {
+                quote = quotes[Math.floor(Math.random() * quotes.length)];
+            } while (out.indexOf(quote) > -1);
+            out[i] = quote;
+        }
+        return out;
+    }
+};
