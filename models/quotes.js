8b3c4457a563448febd1fe5d7c3001a55c17ee67
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-13 12:27:44 +0530

    fix quote repetition, index and header

--- models/quotes.js
+++ models/quotes.js
@@ -118,298 +118,182 @@ module.exports = [
     },
     {
         "id":"30",
-        "quote":"I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain."
-    },
-    {
-        "id":"31",
-        "quote":"Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic."
-    },
-    {
-        "id":"32",
-        "quote":"The mystery of life isn't a problem to solve, but a reality to experience."
-    },
-    {
-        "id":"33",
-        "quote":"What do you despise? By this are you truly known."
-    },
-    {
-        "id":"34",
-        "quote":"There is no escape—we pay for the violence of our ancestors."
-    },
-    {
-        "id":"35",
-        "quote":"When religion and politics travel in the same cart, the riders believe nothing can stand in their way. Their movements become headlong - faster and faster and faster. They put aside all thoughts of obstacles and forget the precipice does not show itself to the man in a blind rush until it's too late."
-    },
-    {
-        "id":"36",
-        "quote":"Hope clouds observation."
-    },
-    {
-        "id":"37",
-        "quote":"It is so shocking to find out how many people do not believe that they can learn, and how many more believe learning to be difficult."
-    },
-    {
-        "id":"38",
-        "quote":"The mind can go either direction under stress—toward positive or toward negative: on or off. Think of it as a spectrum whose extremes are unconsciousness at the negative end and hyperconsciousness at the positive end. The way the mind will lean under stress is strongly influenced by training."
-    },
-    {
-        "id":"39",
-        "quote":"He who controls the spice controls the universe."
-    },
-    {
-        "id":"40",
-        "quote":"Without change something sleeps inside us, and seldom awakens. The sleeper must awaken."
-    },
-    {
-        "id":"41",
-        "quote":"The mind commands the body and it obeys. The mind orders itself and meets resistance."
-    },
-    {
-        "id":"42",
-        "quote":"It is impossible to live in the past, difficult to live in the present and a waste to live in the future."
-    },
-    {
-        "id":"43",
-        "quote":"Fear is the mind-killer."
-    },
-    {
-        "id":"44",
-        "quote":"Greatness is a transitory experience. It is never consistent. It depends in part upon the myth-making imagination of humankind. The person who experiences greatness must have a feeling for the myth he is in. He must reflect what is projected upon him. And he must have a strong sense of the sardonic. This is what uncouples him from belief in his own pretensions. The sardonic is all that permits him to move within himself. Without this quality, even occasional greatness will destroy a man."
-    },
-    {
-        "id":"45",
-        "quote":"A process cannot be understood by stopping it. Understanding must move with the flow of the process, must join it and flow with it."
-    },
-    {
-        "id":"46",
-        "quote":"The people who can destroy a thing, they control it."
-    },
-    {
-        "id":"47",
-        "quote":"Whether a thought is spoken or not it is a real thing and it has power,' Tuek said. 'You might find the line between life and death among the Fremen to be too sharp and quick."
-    },
-    {
-        "id":"48",
-        "quote":"Survival is the ability to swim in strange water."
-    },
-    {
-        "id":"49",
         "quote":"Try looking into that place where you dare not look! You'll find me there, staring out at you!"
     },
     {
-        "id":"50",
-        "quote":"Arrakis teaches the attitude of the knife - chopping off what's incomplete and saying: 'Now, it's complete because it's ended here.'- from 'Collected Sayings of Maud'Dib'' by the Princess Irulan"
-    },
-    {
-        "id":"51",
-        "quote":"The Fremen were supreme in that quality the ancients called 'spannungsbogen' -- which is the self-imposed delay between desire for a thing and the act of reaching out to grasp that thing."
-    },
-    {
-        "id":"52",
-        "quote":"What has mood to do with it? You fight when the necessity arises—no matter the mood! Mood's a thing for cattle or making love or playing the baliset. It's not for fighting."
-    },
-    {
-        "id":"53",
-        "quote":"Once men turned their thinking over to machines in the hope that this would set them free. But that only permitted other men with machines to enslave them."
-    },
-    {
-        "id":"54",
-        "quote":"Highly organized research is guaranteed to produce nothing new."
-    },
-    {
-        "id":"55",
-        "quote":"Any road followed precisely to its end leads precisely nowhere. Climb the mountain just a little bit to test that it's a mountain. From the top of the mountain, you cannot see the mountain."
-    },
-    {
-        "id":"56",
-        "quote":"Proper teaching is recognized with ease. You can know it without fail because it awakens within you that sensation which tells you this is something you have always known."
-    },
-    {
-        "id":"57",
-        "quote":"The willow submits to the wind and prospers until one day it is many willows - a wall against the wind."
-    },
-    {
-        "id":"58",
-        "quote":"There should be a science of discontent. People need hard times to develop psychic muscles. -- Muad'Dib"
-    },
-    {
-        "id":"59",
+        "id":"31",
         "quote":"Give as few orders as possible,' his father had told him once long ago. 'Once you've given orders on a subject, you must always give orders on that subject."
     },
     {
-        "id":"60",
-        "quote":"My father once told me that respect for truth comes close to being the basis for all morality. 'Something cannot emerge from nothing,' he said. This is profound thinking if you understand how unstable 'the truth' can be."
+        "id":"32",
+        "quote":"Respect for truth comes close to being the basis for all morality. Something cannot emerge from nothing. This is profound thinking if you understand how unstable the truth can be."
     },
     {
-        "id":"61",
+        "id":"33",
         "quote":"There is in all things a pattern that is part of our universe. It has symmetry, elegance, and grace - these qualities you find always in that the true artist captures. You can find it in the turning of the seasons, the way sand trails along a ridge, in the branch clusters of the creosote bush of the pattern of its leaves. We try to copy these patterns in our lives and in our society, seeking the rhythms, the dances, the forms that comfort. Yet, it is possible to see peril in the finding of ultimate perfection. It is clear that the ultimate pattern contains its own fixity. In such perfection, all things move towards death."
     },
     {
-        "id":"62",
+        "id":"34",
         "quote":"Beyond a critical point within a finite space, freedom diminishes as numbers increase. This is as true of humans as it is of gas molecules in a sealed flask. The human question is not how many can possibly survive within the system, but what kind of existence is possible for those who so survive."
     },
     {
-        "id":"63",
+        "id":"35",
         "quote":"Any man who retreats into a cave which has only one opening deserves to die."
     },
     {
-        "id":"64",
+        "id":"36",
         "quote":"How often it is that the angry man rages denial of what his inner self is telling him."
     },
     {
-        "id":"65",
+        "id":"37",
         "quote":"There is probably no more terrible instant of enlightenment than the one in which you discover your father is a man - with human flesh."
     },
     {
-        "id":"66",
+        "id":"38",
         "quote":"The concept of progress acts as a protective mechanism to shield us from the terrors of the future."
     },
     {
-        "id":"67",
+        "id":"39",
         "quote":"Anything outside yourself, this you can see and apply your logic to it. But it’s a human trait that when we encounter personal problems, these things most deeply personal are the most difficult to bring out for our logic to scan. We tend to flounder around, blaming everything but the actual, deep-seated thing that’s really chewing on us."
     },
     {
-        "id":"68",
+        "id":"40",
         "quote":"Muad'Dib learned rapidly because his first training was in how to learn. And the first lesson of all was the basic trust that he could learn. It's shocking to find how many people do not believe they can learn, and how many more believe learning to be difficult. Muad'Dib knew that every experience carries its lesson."
     },
     {
-        "id":"69",
+        "id":"41",
         "quote":"A killer with the manners of a rabbit - this is the most dangerous kind."
     },
     {
-        "id":"70",
+        "id":"42",
         "quote":"We faced it and did not resist. The storm passed through us and around us. It's gone, but we remain."
     },
     {
-        "id":"71",
+        "id":"43",
         "quote":"But it's well known that repression makes a religion flourish."
     },
     {
-        "id":"72",
+        "id":"44",
         "quote":"Then, as his planet killed him, it occurred to Kynes that his father and all the other scientists were wrong, that the most persistent principles of the universe were accident and error."
     },
     {
-        "id":"73",
+        "id":"45",
         "quote":"Knowing where the trap is, that's the first step in evading it."
     },
     {
-        "id":"74",
+        "id":"46",
         "quote":"Do you wrestle with dreams? Do you contend with shadows? Do you move in a kind of sleep? Time has slipped away. Your life is stolen. You tarried with trifles, Victim of your folly."
     },
     {
-        "id":"75",
-        "quote":"Many have marked the speed with which Muad'Dib learned the necessities of Arrakis. The Bene Gesserit, of course, know the basis of this speed. For the others, we can say that Muad'Dib learned rapidly because his first training was in how to learn. And the first lesson of all was the basic trust that he could learn. It is shocking to find how many people do not believe they can learn, and how many more believe learning to be difficult. Muad'Dib knew that every experience carries its lesson."
-    },
-    {
-        "id":"76",
+        "id":"47",
         "quote":"Much that was called religion has carried an unconscious attitude of hostility toward life. True religion must teach that life is filled with joys pleasing to the eye of God, that knowledge without action is empty. All men must see that the teaching of religion by rules and rote is largely a hoax. The proper teaching is recognized with ease. You can know it without fail because it awakens within you that sensation which tells you this is something you’ve always known."
     },
     {
-        "id":"77",
+        "id":"48",
         "quote":"You see, gentlemen, they have something to die for. They've discovered they're a people. They're awakening."
     },
     {
-        "id":"78",
+        "id":"49",
         "quote":"What senses do we lack that we cannot see or hear another world all around us?"
     },
     {
-        "id":"79",
+        "id":"50",
         "quote":"And always, he fought the temptation to choose a clear, safe course, warning 'That path leads ever down into stagnation'."
     },
     {
-        "id":"80",
+        "id":"51",
         "quote":"Religion must remain an outlet for people who say to themselves, 'I am not the kind of person I want to be.' It must never sink into an assemblage of the self-satisfied."
     },
     {
-        "id":"81",
+        "id":"52",
         "quote":"Law is the ultimate science."
     },
     {
-        "id":"82",
-        "quote":"Mankind has only one science… its the science of discontent."
+        "id":"53",
+        "quote":"Mankind has only one science.. it's the science of discontent."
     },
     {
-        "id":"83",
+        "id":"54",
         "quote":"Thou shalt not make a machine in the likeness of a human mind."
     },
     {
-        "id":"84",
+        "id":"55",
         "quote":"The vision of time is broad, but when you pass through it, time becomes a narrow door."
     },
     {
-        "id":"85",
+        "id":"56",
         "quote":"Paradise on my right, Hell on my left and the Angel of Death behind."
     },
     {
-        "id":"86",
+        "id":"57",
         "quote":"If you rely only on your eyes, your other senses weaken."
     },
     {
-        "id":"87",
+        "id":"58",
         "quote":"For now is my grief heavier than the sands of the seas, she thought. This world has emptied me of all but the oldest purpose: tomorrow's life."
     },
     {
-        "id":"88",
+        "id":"59",
         "quote":"A ruler must learn to persuade and not to compel."
     },
     {
-        "id":"89",
+        "id":"60",
         "quote":"It's easier to be terrified by an enemy you admire."
     },
     {
-        "id":"90",
+        "id":"61",
         "quote":"When you imagine mistakes, there can be no self-defense."
     },
     {
-        "id":"91",
+        "id":"62",
         "quote":"I am like a person whose hands were kept numb, without sensation from the first moment of awareness - until one day the ability to feel is forced into them. And I say 'Look! I have no hands!' But the people all around me say: 'What are hands?"
     },
     {
-        "id":"92",
+        "id":"63",
         "quote":"The price we paid was the price men have always paid for achieving a paradise in this life--we went soft, we lost our edge."
     },
     {
-        "id":"93",
+        "id":"64",
         "quote":"A stone is heavy and the sand is weighty; but a fool's wrath is heavier than them both."
     },
     {
-        "id":"94",
+        "id":"65",
         "quote":"Humans are almost always lonely."
     },
     {
-        "id":"95",
+        "id":"66",
         "quote":"The thing the ecologically illiterate don't realise about an ecosystem is that it's a system. A system! A system maintains a certain fluid stability that can be destroyed by a misstep in just one niche. A system has order, flowing from point to point. If something dams that flow, order collapses. The untrained might miss that collapse until it was too late. That's why the highest function of ecology is the understanding of consequences."
     },
     {
-        "id":"96",
+        "id":"67",
         "quote":"A man's flesh is his own; the water belongs to the tribe."
     },
     {
-        "id":"97",
+        "id":"68",
         "quote":"No more terrible disaster could befall your people than for them to fall into the hands of a Hero"
     },
     {
-        "id":"98",
+        "id":"69",
         "quote":"Growth is limited by the necessity which is present in the least amount. And naturally, the least favorable condition controls the growth rate"
     },
     {
-        "id":"99",
+        "id":"70",
         "quote":"When he wanted, he could radiate charm and sincerity, but I often wonder in these later days if anything about him was as it seemed. I think now he was a man fighting constantly to escape the bars of an invisible cage."
     },
     {
-        "id":"100",
+        "id":"71",
         "quote":"What is important for a leader is that which makes him a leader. It is the needs of his people."
     },
     {
-        "id":"101",
+        "id":"72",
         "quote":"The test of a man isn’t what you think he’ll do. It’s what he actually does."
     },
     {
-        "id":"102",
+        "id":"73",
         "quote":"I see us giving love to each other in a time of quiet between storms. It's what we were meant to do."
     },
     {
-        "id":"103",
+        "id":"74",
         "quote":"A popular man arouses the jealousy of the powerful."
     }
 ]
