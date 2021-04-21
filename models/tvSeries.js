503f7ba1f59b64f30797568172881f86acf4490c
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-21 12:23:21 +0530

    add series data, routes

---
+++ models/tvSeries.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = [
+    {
+        "id" : "1",
+        "title": "Frank Herbert's Dune",
+        "year": "2000",
+        "director": "John Harrison",
+        "wiki_url": "https://en.wikipedia.org/wiki/Frank_Herbert%27s_Dune"
+    },
+    {
+        "id" : "2",
+        "title": "Frank Herbert's Children of Dune",
+        "year": "2003",
+        "director": "Greg Yaitanes",
+        "wiki_url": "https://en.wikipedia.org/wiki/Frank_Herbert%27s_Children_of_Dune"
+    }
+]
