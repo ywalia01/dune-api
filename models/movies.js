8a03dd6f3325171af90993a1c086c10d89e6aafa
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-04-21 12:20:31 +0530

    add movie data, routes

---
+++ models/movies.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = [
+    {
+        "id" : "1",
+        "title": "Dune",
+        "year": "1984",
+        "director": "David Lynch",
+        "wiki_url": "https://en.wikipedia.org/wiki/Dune_(1984_film)"
+    },
+    {
+        "id" : "2",
+        "title": "Dune",
+        "year": "2021",
+        "director": "Denis Villeneuve",
+        "wiki_url": "https://en.wikipedia.org/wiki/Dune_(2021_film)"
+    }
+]
